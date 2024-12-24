d5782468490d422b5ae5eaff695e59429e505d3d
Swallows-annzi <1598773037@qq.com>
2024-12-25 01:24:02 +0800

    随机数生成代码修正

--- src/main/java/com/swallows/additionalapi/crafttweaker/util/SaaSecureRandom.java
+++ src/main/java/com/swallows/additionalapi/crafttweaker/util/SaaSecureRandom.java
@@ -12,63 +12,63 @@ public class SaaSecureRandom {
     static SecureRandom secureRandom = new SecureRandom();
 
     @ZenMethod
-    public static int nextInt () {
+    public static int nextInt() {
         return secureRandom.nextInt();
     }
 
     @ZenMethod
-    public static int nextInt (int maximum) {
+    public static int nextInt(int maximum) {
         return secureRandom.nextInt(maximum);
     }
 
     @ZenMethod
-    public static int nextInt (int minimum, int maximum) {
-        return secureRandom.nextInt(minimum, maximum);
+    public static int nextInt(int minimum, int maximum) {
+        return minimum + secureRandom.nextInt(maximum - minimum);
     }
 
     @ZenMethod
-    public static long nextLong () {
+    public static long nextLong() {
         return secureRandom.nextLong();
     }
 
     @ZenMethod
-    public static long nextLong (long maximum) {
-        return secureRandom.nextLong(maximum);
+    public static long nextLong(long maximum) {
+        return Math.abs(secureRandom.nextLong()) % maximum;
     }
 
     @ZenMethod
-    public static long nextLong (long minimum, long maximum) {
-        return secureRandom.nextLong(minimum, maximum);
+    public static long nextLong(long minimum, long maximum) {
+        return minimum + Math.abs(secureRandom.nextLong()) % (maximum - minimum);
     }
 
     @ZenMethod
-    public static float nextFloat () {
+    public static float nextFloat() {
         return secureRandom.nextFloat();
     }
 
     @ZenMethod
-    public static float nextFloat (float maximum) {
-        return secureRandom.nextFloat(maximum);
+    public static float nextFloat(float maximum) {
+        return secureRandom.nextFloat() * maximum;
     }
 
     @ZenMethod
-    public static float nextFloat (float minimum, float maximum) {
-        return secureRandom.nextFloat(minimum, maximum);
+    public static float nextFloat(float minimum, float maximum) {
+        return minimum + secureRandom.nextFloat() * (maximum - minimum);
     }
 
     @ZenMethod
-    public static double nextDouble () {
+    public static double nextDouble() {
         return secureRandom.nextDouble();
     }
 
     @ZenMethod
-    public static double nextDoublet (double maximum) {
-        return secureRandom.nextDouble(maximum);
+    public static double nextDouble(double maximum) {
+        return secureRandom.nextDouble() * maximum;
     }
 
     @ZenMethod
-    public static double nextDouble (double minimum, double maximum) {
-        return secureRandom.nextDouble(minimum, maximum);
+    public static double nextDouble(double minimum, double maximum) {
+        return minimum + secureRandom.nextDouble() * (maximum - minimum);
     }
 
     @ZenMethod
@@ -82,12 +82,12 @@ public class SaaSecureRandom {
     }
 
     @ZenMethod
-    public static double nextGaussian (double mean, double stdDev) {
-        return secureRandom.nextGaussian(mean, stdDev);
+    public static double nextGaussian(double mean, double stdDev) {
+        return mean + stdDev * secureRandom.nextGaussian();
     }
 
     @ZenMethod
-    public static double nextExponential () {
-        return secureRandom.nextExponential();
+    public static double nextExponential(double lambda) {
+        return -Math.log(1.0 - secureRandom.nextDouble()) / lambda;
     }
 }
